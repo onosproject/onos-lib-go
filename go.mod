cdb05d73f2ffce8cecd16236ff1dd5ddf693ce03
Sean Condon <sean@opennetworking.org>
2020-02-21 12:25:24 +0000

    Added in some base functions for CLI that are common to other modules

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/onosproject/onos-lib-go
 go 1.13
 
 require (
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/plar/go-adaptive-radix-tree v1.0.1
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.5.0
 	github.com/stretchr/testify v1.4.0
 	go.uber.org/zap v1.13.0
 	golang.org/x/tools v0.0.0-20191029190741-b9c20aec41a5
 	google.golang.org/grpc v1.27.1
+	gotest.tools v2.2.0+incompatible
 )
---
+++ pkg/cli/cli_test.go
@@ -0,0 +1,61 @@
+// Copyright 2020-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"github.com/spf13/cobra"
+	"gotest.tools/assert"
+	"testing"
+)
+
+// These are mainly placeholders for the moment for functions that are not
+// otherwise called
+func Test_InitConfig(t *testing.T) {
+	initConfig("test-module")
+
+	assert.Equal(t, "test-module", configName)
+}
+
+func Test_GetConnection(t *testing.T) {
+	conn, err := getConnection(&cobra.Command{
+		Short: "test command",
+	})
+	assert.NilError(t, err)
+	assert.Assert(t, conn != nil)
+}
+
+func Test_AddConfigFlags(t *testing.T) {
+	addConfigFlags(&cobra.Command{
+		Short: "test command",
+	})
+}
+
+func Test_GetConfigCommand(t *testing.T) {
+	getConfigCommand()
+}
+
+func Test_RunConfigSetCommand(t *testing.T) {
+	err := runConfigSetCommand(&cobra.Command{
+		Short: "test command",
+	}, []string{"a", "b", "c"})
+	assert.ErrorContains(t, err, "Config File \"test-module\" Not Found")
+}
+
+func Test_GetCertPath(t *testing.T) {
+	path := getCertPath(&cobra.Command{
+		Short: "test command",
+	})
+	assert.Assert(t, len(path) == 0)
+}
