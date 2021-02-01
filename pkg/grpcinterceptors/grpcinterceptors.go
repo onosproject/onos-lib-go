47df1f7c46dad6be7401c3257fdbab851ce1439e
Sean Condon <sean@opennetworking.org>
2021-02-01 10:53:20 +0000

    Authentication: improve logging messages

--- pkg/grpcinterceptors/grpcinterceptors.go
+++ pkg/grpcinterceptors/grpcinterceptors.go
@@ -34,8 +34,6 @@ var log = logging.GetLogger("interceptors")
 
 // AuthenticationInterceptor an interceptor for authentication
 func AuthenticationInterceptor(ctx context.Context) (context.Context, error) {
-	log.Info("Authenticating the user")
-
 	// Extract token from metadata in the context
 	tokenString, err := grpc_auth.AuthFromMD(ctx, ContextMetadataTokenKey)
 	if err != nil {
