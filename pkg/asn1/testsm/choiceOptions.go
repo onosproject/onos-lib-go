d4b25b9d72170a8b0725ffb6332bb5d9b5e3e3fe
Sean Condon <sean@opennetworking.org>
2021-07-23 11:09:09 +0100

    Import test_sm proto from e2-sm (#137)

---
+++ pkg/asn1/testsm/choiceOptions.go
@@ -0,0 +1,52 @@
+// Copyright 2021-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testsm
+
+import "reflect"
+
+// Choicemap - Should be generated with protoc-gen-choice from proto
+var Choicemap = map[string]map[int]reflect.Type{
+	"choice1": {
+		1: reflect.TypeOf(Choice1_Choice1A{}),
+	},
+	"choice2": {
+		1: reflect.TypeOf(Choice2_Choice2A{}),
+		2: reflect.TypeOf(Choice2_Choice2B{}),
+	},
+	"choice3": {
+		1: reflect.TypeOf(Choice3_Choice3A{}),
+		2: reflect.TypeOf(Choice3_Choice3B{}),
+		3: reflect.TypeOf(Choice3_Choice3C{}),
+	},
+	"choice4": {
+		1: reflect.TypeOf(Choice4_Choice4A{}),
+	},
+	"constrained_choice1": {
+		1: reflect.TypeOf(ConstrainedChoice1_ConstrainedChoice1A{}),
+	},
+	"constrained_choice2": {
+		1: reflect.TypeOf(ConstrainedChoice2_ConstrainedChoice2A{}),
+		2: reflect.TypeOf(ConstrainedChoice2_ConstrainedChoice2B{}),
+	},
+	"constrained_choice3": {
+		1: reflect.TypeOf(ConstrainedChoice3_ConstrainedChoice3A{}),
+		2: reflect.TypeOf(ConstrainedChoice3_ConstrainedChoice3B{}),
+		3: reflect.TypeOf(ConstrainedChoice3_ConstrainedChoice3C{}),
+		4: reflect.TypeOf(ConstrainedChoice3_ConstrainedChoice3D{}),
+	},
+	"constrained_choice4": {
+		1: reflect.TypeOf(ConstrainedChoice4_ConstrainedChoice4A{}),
+	},
+}
---
+++ pkg/asn1/testsm/test_sm_encode_choice_test.go
@@ -0,0 +1,70 @@
+// Copyright 2021-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testsm
+
+import (
+	"encoding/hex"
+	"github.com/onosproject/onos-lib-go/pkg/asn1/aper"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_encodeChoice1(t *testing.T) {
+	testChoice1 := &TestChoices{
+		OtherAttr: "choice1only",
+		Choice1: &Choice1{
+			Choice1: &Choice1_Choice1A{
+				Choice1A: 10,
+			},
+		},
+		Choice2: &Choice2{
+			Choice2: &Choice2_Choice2B{
+				Choice2B: 20,
+			},
+		},
+		Choice3: &Choice3{
+			Choice3: &Choice3_Choice3B{
+				Choice3B: 30,
+			},
+		},
+		Choice4: &Choice4{
+			Choice4: &Choice4_Choice4A{
+				Choice4A: 10,
+			},
+		},
+	}
+	tcExpected := []byte{
+		0x0b,                                                             // The length of the following text == 11 - not constrained so uses all 8 bits
+		0x63, 0x68, 0x6f, 0x69, 0x63, 0x65, 0x31, 0x6f, 0x6e, 0x6c, 0x79, // the text "choice1only"
+		0x00, // Choice 1a (1 of 1) 1 bit and 0 for extensible TODO shouldn't this be 0 bits for choice
+		0x01, // num bytes for integer - 0000 0001 - not constrained so uses all 8 bits
+		0x0a, // Value of Choice 1a int = 10
+		0x80, // Choice 2b (2 of 2) 2 bits for choice, 0 for extensible TODO shouldn't this be 1 bit for choice
+		0x01, // num bytes for integer - 0000 0001 - not constrained so uses all 8 bits
+		0x14, // Value of Choice 2b int = 20
+		0x40, // Choice 3b (2 of 3) 3 bits for choice, 0 for extensible TODO shouldn't this be 2 bits for choice
+		0x01, // num bytes for integer - 0000 0001 - not constrained so uses all 8 bits
+		0x1e, // Value of Choice 3b int = 30
+		0x00, // Choice 4a (1 of 1) 1 bit for choice, 0 for extensible TODO shouldn't this be 0 bit for choice
+		0x01, // num bytes for integer - 0000 0001 - not constrained so uses all 8 bits
+		0x0a, // Value of Choice 4a int = 10
+	}
+
+	aper, err := aper.Marshal(testChoice1)
+	assert.NoError(t, err)
+	assert.NotNil(t, aper)
+	t.Logf("Choice 1 APER %s", hex.Dump(aper))
+	assert.EqualValues(t, tcExpected, aper)
+}
