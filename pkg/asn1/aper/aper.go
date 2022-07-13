c26524c91b38ed65ac24bcda25f0c874c1b54b2f
Ivan Eroshkin <erijj@mail.ru>
2022-07-13 17:55:22 +0200

    Setting log level back

--- pkg/asn1/aper/aper.go
+++ pkg/asn1/aper/aper.go
@@ -1192,7 +1192,7 @@ func Unmarshal(b []byte, value interface{}, choiceMap map[string]map[int]reflect
 // UnmarshalWithParams allows field parameters to be specified for the
 // top-level element. The form of the params is the same as the field tags.
 func UnmarshalWithParams(b []byte, value interface{}, params string, choiceMap map[string]map[int]reflect.Type, canonicalChoiceMap map[string]map[int64]reflect.Type) error {
-	log.SetLevel(logging.DebugLevel)
+	//log.SetLevel(logging.DebugLevel)
 	v := reflect.ValueOf(value).Elem()
 	pd := &perBitData{b, 0, 0, choiceMap, -1, false, canonicalChoiceMap, false}
 	err := parseField(v, pd, parseFieldParameters(params))
